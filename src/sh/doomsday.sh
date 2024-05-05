dd81e70c1bd8126694b2b973031ed75c51780108
patrickamadeus <patrickai0309@gmail.com>
2024-05-05 03:12:39 +0000

    nitpick eval script

--- src/sh/doomsday.sh
+++ src/sh/doomsday.sh
@@ -1,16 +1,12 @@
 ## Remove old environments & kernels
 jupyter kernelspec uninstall main_env
 conda remove --name main_env --all
-# jupyter kernelspec uninstall eval_env
-# conda remove --name eval_env --all
 
 # ## Create environment from .yml file
 conda env create -f ./yml/main.yml
-# conda env create -f ./yml/eval.yml
 
 ## Install ipykernel for further usage
 python -m ipykernel install --user --name main_env --display-name "Patrick (Main)"
-# python -m ipykernel install --user --name eval_env --display-name "Patrick (Eval)"
 
 ## Download environment's model by running script
 cd scripts
